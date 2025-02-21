bb7a96359352d69ab4590415d4737c78c99a3a50
Nikile <150235099+Nikile2003@users.noreply.github.com>
2025-02-21 23:08:20 +0530

    Add files via upload

---
+++ task8.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE ecommerce;
+USE ecommerce;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    category VARCHAR(100),
+    stock INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE products ADD COLUMN image VARCHAR(255);
+
+SELECT*FROM PRODUCTS;
